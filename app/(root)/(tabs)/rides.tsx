221e7f7ee12793eaa98baba4739f911538686e46
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-11-11 12:07:21 +0200

    Display recent rides with improved UI and data fetch
    - Replaced static content with `FlatList` to render recent rides dynamically.
    - Integrated `useFetch` hook to fetch user's recent rides from API.
    - Added `RideCard` component to display ride details.
    - Enhanced empty state with a placeholder view and loader for better UX.

--- app/(root)/(tabs)/rides.tsx
+++ app/(root)/(tabs)/rides.tsx
@@ -1,10 +1,55 @@
-import {Text} from "react-native";
+import {Fragment} from "react";
+import {useUser} from "@clerk/clerk-expo";
+import {ActivityIndicator, FlatList, Image, Text, View} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
+import {RideCard} from "../../../components/RideCard";
+import {images} from "../../../constants";
+import {useFetch} from "../../../lib/fetch";
+import {Ride} from "../../../types/type";
 
 function ScreenRides() {
+  const {user} = useUser();
+  const {data: recentRides, loading} = useFetch<Ride[]>(
+    `/(api)/ride/${user?.id}`,
+  );
+
   return (
-    <SafeAreaView>
-      <Text>Rides</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={recentRides}
+        renderItem={({item}) => <RideCard ride={item} />}
+        keyExtractor={(item, index) => index.toString()}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!loading ? (
+              <Fragment>
+                <Image
+                  source={images.noResult}
+                  className="h-40 w-40"
+                  alt="No recent rides found"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm">No recent rides found</Text>
+              </Fragment>
+            ) : (
+              <ActivityIndicator
+                size="small"
+                color="#000"
+              />
+            )}
+          </View>
+        )}
+        ListHeaderComponent={
+          <Fragment>
+            <Text className="my-5 font-JakartaBold text-2xl">All Rides</Text>
+          </Fragment>
+        }
+      />
     </SafeAreaView>
   );
 }
